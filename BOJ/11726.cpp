000e2b3b567e4895cd3232077ffc2070c3a38384
candi1004 <rlaehdwn3331@naver.com>
2017-07-04 12:38:23 +0900

    11726 2*n 타일링

---
+++ BOJ/11726.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+using namespace std;
+
+int n, d[1001];
+int go(int k);
+int main() {
+	scanf("%d", &n);
+	printf("%d\n", go(n));
+	return 0;
+}
+int go(int k) {
+	if (d[k] > 0) {
+		return d[k];
+	}
+	if (k == 1) return 1;
+	if (k == 2) return 2;
+	d[k] = go(k - 1) + go(k - 2);
+	return d[k];
+}
